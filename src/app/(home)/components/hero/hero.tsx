9e373e31379357b7ede43bc8ace21017af62577e
David Acevedo <acevedo.posada@hotmail.com>
2024-05-12 01:07:22 -0500

    :construction: Add new scroll animation

--- src/app/(home)/components/hero/hero.tsx
+++ src/app/(home)/components/hero/hero.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Lottie from 'react-lottie';
 
 import scrollAnimation from '@/components/animations/scroll-animation.json';
@@ -10,73 +10,79 @@ import styles from './hero.module.scss';
 import { cn } from '@/utils/cn';
 
 export default function Hero() {
+  const { scrollYProgress } = useScroll();
+
+  const transformText = useTransform(scrollYProgress, [0, 1], [1, 0.5]);
+
   return (
     <div className={styles.hero}>
       <AuroraBackground className='bg-transparent z-30 pointer-events-none opacity-40' />
       <HeroHighlight containerClassName='h-dvh absolute top-0 left-0'>
-        <h1 className={styles.hero__title}>
-          <span
-            className={cn(
-              'text-5xl md:text-6xl lg:text-[6rem]',
-              styles.hero__title__section
-            )}
-          >
-            <motion.span
-              className='inline-block'
-              initial={{ y: '100%' }}
-              animate={{ y: 0 }}
+        <motion.div style={{ scale: transformText }}>
+          <motion.h1 className={styles.hero__title}>
+            <span
+              className={cn(
+                'text-5xl md:text-6xl lg:text-[6rem]',
+                styles.hero__title__section
+              )}
             >
-              I&apos;m
-            </motion.span>{' '}
-            <motion.span
-              className={cn('inline-block', styles.hero__title__highlight)}
-              initial={{ y: '100%' }}
-              animate={{ y: 0 }}
-              transition={{ delay: 0.1 }}
-            >
-              David
-            </motion.span>
-          </span>
-          <span className={cn('lg:-mb-6', styles.hero__title__section)}>
-            <Highlight>
               <motion.span
                 className='inline-block'
                 initial={{ y: '100%' }}
                 animate={{ y: 0 }}
-                transition={{ delay: 0.3 }}
               >
-                Creative
+                I&apos;m
               </motion.span>{' '}
               <motion.span
+                className={cn('inline-block', styles.hero__title__highlight)}
+                initial={{ y: '100%' }}
+                animate={{ y: 0 }}
+                transition={{ delay: 0.1 }}
+              >
+                David
+              </motion.span>
+            </span>
+            <span className={cn('lg:-mb-6', styles.hero__title__section)}>
+              <Highlight>
+                <motion.span
+                  className='inline-block'
+                  initial={{ y: '100%' }}
+                  animate={{ y: 0 }}
+                  transition={{ delay: 0.3 }}
+                >
+                  Creative
+                </motion.span>{' '}
+                <motion.span
+                  className='inline-block'
+                  initial={{ y: '100%' }}
+                  animate={{ y: 0 }}
+                  transition={{ delay: 0.4 }}
+                >
+                  Frontend
+                </motion.span>
+              </Highlight>
+            </span>
+            <span className={styles.hero__title__section}>
+              <motion.span
                 className='inline-block'
                 initial={{ y: '100%' }}
                 animate={{ y: 0 }}
-                transition={{ delay: 0.4 }}
+                transition={{ delay: 0.5 }}
               >
-                Frontend
+                Developer.
               </motion.span>
-            </Highlight>
-          </span>
-          <span className={styles.hero__title__section}>
-            <motion.span
-              className='inline-block'
-              initial={{ y: '100%' }}
-              animate={{ y: 0 }}
-              transition={{ delay: 0.5 }}
-            >
-              Developer.
-            </motion.span>
-          </span>
-        </h1>
-        <motion.p
-          className={styles.hero__description}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.6, delay: 1 }}
-        >
-          I develop accessible, responsive, interactive, and animated websites
-          and applications with a strong focus on performance.
-        </motion.p>
+            </span>
+          </motion.h1>
+          <motion.p
+            className={styles.hero__description}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.6, delay: 1 }}
+          >
+            I develop accessible, responsive, interactive, and animated websites
+            and applications with a strong focus on performance.
+          </motion.p>
+        </motion.div>
       </HeroHighlight>
       <motion.div
         className={styles.hero__scroll}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,30 @@
+'use client';
+
 import { Hero } from './components';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 export default function Page() {
+  const { scrollY } = useScroll();
+
+  const innerHeight = typeof window !== 'undefined' && window.innerHeight;
+
+  const boxY = useTransform(
+    scrollY,
+    [0, innerHeight as number],
+    [0, innerHeight as number]
+  );
+
   return (
     <main>
-      <Hero />
+      <div className='h-dvh relative z-[1]'>
+        <motion.section
+          className='absolute w-full left-0'
+          style={{ top: boxY }}
+        >
+          <Hero />
+        </motion.section>
+      </div>
+      <section className='h-[200dvh] bg-black relative z-10'></section>
     </main>
   );
 }

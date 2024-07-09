c29c4e74c0e7e962954a4813e4002947b590227b
David Acevedo <acevedo.posada@hotmail.com>
2024-07-09 01:22:21 -0500

    :bug: Fix Layout type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Ubuntu, Lora } from "next/font/google";
 import type { Metadata } from "next";
-import "./globals.css";
 import { cn } from "@/utils/cn";
+import "./globals.css";
 
 const ubuntu = Ubuntu({
   subsets: ["latin"],
@@ -21,10 +21,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  modals,
 }: Readonly<{
   children: React.ReactNode;
-  modals: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -33,7 +31,6 @@ export default function RootLayout({
       </head>
       <body className={cn(ubuntu.className, lora.variable, "bg-alt")}>
         {children}
-        {modals}
       </body>
     </html>
   );

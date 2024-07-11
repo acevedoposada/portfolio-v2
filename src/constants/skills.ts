4258c8ffa4a21454ee1f0d38b6173463540d0507
David Acevedo <acevedo.posada@hotmail.com>
2024-07-10 22:34:40 -0500

    :recycle: improve footer performance

---
+++ src/constants/skills.ts
@@ -0,0 +1,90 @@
+export const skills = [
+  {
+    image:
+      "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+    title: "ReactJS",
+    link: "https://react.dev/",
+    colors: [
+      [4, 217, 251],
+      [156, 247, 251],
+    ],
+  },
+  {
+    image:
+      "https://static-00.iconduck.com/assets.00/nextjs-icon-512x512-y563b8iq.png",
+    title: "NextJS",
+    link: "https://nextjs.org/",
+    colors: [
+      [240, 240, 240],
+      [255, 255, 255],
+    ],
+    classes: {
+      overlay: "invert",
+    },
+  },
+  {
+    image:
+      "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/21_Angular_logo_logos-512.png",
+    title: "Angular",
+    link: "https://angular.dev/",
+    colors: [
+      [236, 72, 153],
+      [232, 121, 249],
+    ],
+  },
+  {
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/1024px-Tailwind_CSS_Logo.svg.png",
+    title: "TailwindCSS",
+    link: "https://tailwindcss.com/",
+    colors: [[52, 188, 251]],
+  },
+  {
+    image:
+      "https://cdn.iconscout.com/icon/free/png-256/free-firebase-11796860-9632965.png",
+    title: "Firebase",
+    link: "https://firebase.google.com/",
+    colors: [
+      [245, 133, 11],
+      [251, 203, 61],
+    ],
+  },
+  {
+    image: "https://cdn.worldvectorlogo.com/logos/strapi-2.svg",
+    title: "Strapi",
+    link: "https://strapi.io/",
+    colors: [
+      [140, 116, 252],
+      [164, 147, 252],
+    ],
+  },
+  {
+    image: "https://cdn.worldvectorlogo.com/logos/lit-1.svg",
+    title: "LitElement",
+    link: "https://lit.dev/",
+    colors: [
+      [52, 76, 252],
+      [4, 241, 252],
+    ],
+  },
+  {
+    image:
+      "https://seeklogo.com/images/F/framer-motion-logo-DA1E33CAA1-seeklogo.com.png",
+    title: "Framer Motion",
+    link: "https://www.framer.com/motion/",
+    colors: [
+      [188, 76, 148],
+      [91, 83, 156],
+    ],
+    classes: {
+      title: "lg:-mt-10",
+    },
+  },
+  {
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png",
+    title: "Typescript",
+    link: "https://www.typescriptlang.org/",
+    colors: [[51, 123, 196]],
+  },
+];

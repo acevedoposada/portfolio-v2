89425ab39b980afafc508babc3179c7ffc6c889c
David Acevedo <acevedo.posada@hotmail.com>
2025-08-12 17:28:34 -0500

    :sparkles: Improve skill cards ui

--- src/app/(home)/components/skills-section/skills-section.tsx
+++ src/app/(home)/components/skills-section/skills-section.tsx
@@ -17,14 +17,14 @@ export default function SkillsSection() {
         {skills.map((skill, idx) => (
           <a key={idx} href={skill.link} target="_blank">
             <PixelCard className="group mx-auto" colors={getColors(skill.colors)}>
-              <div className="opacity-0 group-hover:opacity-100 absolute top-0 left-0 h-full w-full grid place-content-center transition-opacity duration-300">
+              <div className="opacity-0 group-hover:opacity-100 absolute top-0 left-0 h-full w-full grid items-end pb-6 md:pb-8 -translate-y-7 group-hover:translate-y-0 transition-all duration-300">
                 <h3
-                  className="text-lg md:text-5xl xl:text-3xl text-center text-transparent [-webkit-text-stroke-color:white] [-webkit-text-stroke-width:0.5px] leading-none group-hover/canvas-card:text-transparent bg-gradient-to-tr from-white to-transparent bg-clip-text pb-1"
+                  className="text-lg md:text-4xl xl:text-3xl text-center text-transparent [-webkit-text-stroke-color:white] [-webkit-text-stroke-width:0.5px] leading-none group-hover/canvas-card:text-transparent bg-gradient-to-tr from-white to-transparent bg-clip-text pb-1"
                 >
                   {skill.title}
                 </h3>
               </div>
-              <div className="group-hover:opacity-0 absolute top-0 left-0 w-full h-full grid place-content-center transition-opacity duration-300">
+              <div className="group-hover:-translate-y-4 md:group-hover:-translate-y-6 absolute top-0 left-0 w-full h-full grid place-content-center transition-transform duration-300">
                 <div className="relative z-20 mx-auto flex h-10/12 w-10/12 2xl:w-full 2xl:h-full items-center justify-center text-center transition duration-200">
                   <Image
                     className="relative z-20"

2ba841010fc82470674ba617ab4d7a429aec775e
David Acevedo <acevedo.posada@hotmail.com>
2025-08-12 00:16:14 -0500

    ♻️ Refactor classes, pass to scss files to better readeability

--- src/app/(home)/components/floating-navbar/floating-navbar.tsx
+++ src/app/(home)/components/floating-navbar/floating-navbar.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { AnimatePresence, motion } from "framer-motion";
+import { IconArrowUpRight } from "@tabler/icons-react";
+import Link from "next/link";
 
 import { cn } from "@/utils/cn";
 
 import { NavButtonProps, FloatingNavbarProps } from "./floating-navbar.entity";
-import Link from "next/link";
+import styles from './floating-navbar.module.scss'
 
 function NavButton({ isActive, children, element, icon, link }: NavButtonProps) {
   function handleClick() {
@@ -23,27 +25,36 @@ function NavButton({ isActive, children, element, icon, link }: NavButtonProps) 
   return (
     <Component
       className={cn(
-        "group relative flex items-center justify-center gap-1 rounded-full border border-transparent px-4 py-2 text-sm font-medium text-white transition-all w-full md:w-auto",
-        isActive && "border-white/[0.2]",
+        "group",
+        styles.fn__btn,
+        isActive && styles['fn__btn--active'],
       )}
       onClick={handleClick}
     >
+      {
+        link && (
+          <span className={cn(styles.fn__btn__link, 'group-hover:scale-100')}>
+            <IconArrowUpRight size={16} />
+          </span>
+        )
+      }
       <span
         className={cn(
-          "flex h-6 w-6 items-center justify-center opacity-60 transition-all [&>svg]:h-5 [&>svg]:w-5 md:[&>svg]:h-4 md:[&>svg]:w-4 [&>svg]:transition-all",
-          isActive && "opacity-100 [&>svg]:h-6 [&>svg]:w-6 md:[&>svg]:h-5 md:[&>svg]:w-5",
+          'group-hover:opacity-100',
+          styles.fn__btn__icon,
+          isActive && styles['fn__btn__icon--active'],
         )}
       >
         {icon}
       </span>
-      <span className="inset-0 hidden overflow-x-hidden text-sm sm:block">
+      <span className={styles.fn__btn__label}>
         {children}
       </span>
       <span
         className={cn(
-          "absolute inset-x-0 -bottom-px mx-auto h-px w-0 bg-gradient-to-r from-transparent via-blue-300 to-transparent transition-all group-hover:w-2/3",
-          isActive &&
-          "w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent",
+          "group-hover:w-2/3",
+          styles.fn__btn__indicator,
+          isActive && styles['fn__btn__indicator--active'],
         )}
       />
     </Component>
@@ -63,7 +74,7 @@ export default function FloatingNavbar({
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.2 }}
         className={cn(
-          "fixed inset-x-0 bottom-5 md:bottom-10 z-[5000] mx-auto flex md:max-w-fit w-[calc(100%_-_4rem)] items-center justify-between md:space-x-2 rounded-full border border-white/[0.2] bg-black px-2 py-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]",
+          styles.fn,
           className,
         )}
       >

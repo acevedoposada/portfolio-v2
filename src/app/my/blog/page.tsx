cde9e4700d544f06fe8bb94c8f4f676fc4d4057e
David Acevedo <acevedo.posada@hotmail.com>
2025-08-20 02:09:01 -0500

    💅 Improve spacings

--- src/app/my/blog/page.tsx
+++ src/app/my/blog/page.tsx
@@ -10,7 +10,7 @@ export default function Blog() {
       </div>
       <BlogHeader />
 
-      <div className="container px-6 py-14 flex flex-col gap-12">
+      <div className="container px-6 pb-14 pt-10 md:pt-14 flex flex-col gap-12">
         <BlogCard
           variant='featured'
           image="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?q=80&w=4076&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"

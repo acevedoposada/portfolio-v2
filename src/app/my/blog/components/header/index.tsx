ef6a330c64f1bc498bf3817d921e0b889cdb2bc4
David Acevedo <acevedo.posada@hotmail.com>
2025-08-25 19:17:31 -0500

    :sparkles: Add scroll appear header when user scroll down

--- src/app/my/blog/components/header/index.tsx
+++ src/app/my/blog/components/header/index.tsx
@@ -1,24 +1,30 @@
 "use client";
 
+import { AnimatePresence, motion, Variants } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { IconChevronLeft } from "@tabler/icons-react";
+import { memo } from "react";
+import Link from "next/link";
+
 import { Highlight } from "@/components";
 import { Chip } from "@/components/ui/chip";
-import Link from "next/link";
-import { useEffect } from "react";
-import { useInView } from "react-intersection-observer";
 
-export default function BlogHeader(): JSX.Element {
-  const { ref, inView: isInView, entry } = useInView({
+import styles from './header.module.scss';
+
+const variants: Variants = {
+  enter: { y: 0, opacity: 1 },
+  exit: { y: -50, opacity: 0 },
+}
+
+const BlogHeader = memo((): JSX.Element => {
+  const { ref, inView: isInView } = useInView({
     threshold: 0,
     rootMargin: "-200px 0px 0px 0px"
   })
 
-  useEffect(() => {
-    console.log("In view state: ", isInView, entry)
-  }, [isInView])
-
   return (
     <header ref={ref} className="pt-12">
-      <div className="container px-6 flex flex-col gap-6 ">
+      <div className={styles.header}>
         <Link href="/">
           Go home
         </Link>
@@ -30,15 +36,36 @@ export default function BlogHeader(): JSX.Element {
           Read my blog
         </Chip>
         <h1 className="text-4xl lg:text-6xl font-lora font-medium">
-          Stack of <Highlight className="px-2 lg:px-4">Ideas</Highlight>
+          Thought <Highlight className="px-2 lg:px-4">Journal</Highlight>
         </h1>
         <p className="text-lg lg:text-2xl font-light opacity-80">
           A blog about development, tech trends, and best practices to help you write better code and understand the digital future.
         </p>
       </div>
-      {!isInView && (
-        <div className="fixed h-20 w-full top-0 left-0 z-10 bg-indigo-500/20 backdrop-blur-xl"></div>
-      )}
+      <AnimatePresence>
+        {!isInView && (
+          <motion.div
+            className={styles.header__fixed}
+            variants={variants}
+            initial="exit"
+            animate="enter"
+            exit="exit"
+          >
+            <div className="container px-6 py-6 flex items-center">
+              <Link href="/" className="p-1.5 pl-0 rounded-lg peer transition-all duration-300 hover:bg-indigo-600/25 hover:pl-1.5">
+                <IconChevronLeft />
+              </Link>
+              <h2 className="text-xl lg:text-3xl font-lora font-medium peer-hover:ml-4 transition-all duration-300">
+                Thought <Highlight>Journal</Highlight>
+              </h2>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   )
-}
+})
+
+BlogHeader.displayName = 'BlogHeader';
+
+export default BlogHeader
